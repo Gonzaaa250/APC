ccc811231ac52e181fa82a3fac16de59148bed99
Gonzaaa <gonzaavalero25@gmail.com>
2023-06-26 21:16:44 -0300

    Controlador y vista Ranking

--- Data/TesisPadelDbContext.cs
+++ Data/TesisPadelDbContext.cs
@@ -12,4 +12,5 @@ public class TesisPadelDbContext: DbContext{
     // public DbSet <Torneo> Torneos {get; set;}
     public DbSet<Localidad>? Localidad { get; set; }
     public DbSet<Provincia>? Provincia { get; set; }
+    public DbSet<TesisPadel.Models.Ranking> Ranking { get; set; }
 }
--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -6,7 +6,38 @@ public class Categoria{
     
     public virtual ICollection<Ranking>? Rankings {get; set;}
 }
+public class Enum{
+     public enum Opcion
+    {
+        Masculino,
+        Femenino
+    }
 
+    public Opcion Categoria { get; set; }
+    Enum miEnum = new Enum();
+
+// Console.WriteLine("Elige una opción: ");
+// Console.WriteLine("1. Masculino");
+// Console.WriteLine("2. Femenino");
+
+// int opcion = Convert.ToInt32(Console.ReadLine());
+
+// switch (opcion)
+// {
+//     case 1:
+//         miEnum.Categoria = Enum.Opcion.Masculino;
+//         break;
+//     case 2:
+//         miEnum.Categoria = Enum.Opcion.Femenino;
+//         break;
+//     default:
+//         Console.WriteLine("Opción inválida");
+//         break;
+// }
+
+// Console.WriteLine("La opción elegida es: " + miEnum.Categoria);
+
+}
 
 
 
